0b32557b327f7caea2bfe3b65bbaf6962194382b
Emily Choi <echoi011@student.fuhsd.org>
2022-05-11 18:30:32 -0700

    hopefully no merge confliict

--- CapstoneProject/src/Obstacle/Obstacle.java
+++ CapstoneProject/src/Obstacle/Obstacle.java
@@ -10,14 +10,8 @@ import Player.Player;
  * @author Claire
  *
  */
-<<<<<<< Updated upstream
-public class Obstacle {
-	
-	private boolean ranInto;
-	private double x, y;
-=======
+
 public class Obstacle extends Element{
->>>>>>> Stashed changes
 	
 	/**
 	 * Constructs an obstacle
@@ -37,44 +31,4 @@ public class Obstacle extends Element{
 		p.moveByAmount(-10, 0);
 	}
 	
-	/**
-	 * Receies the boundaries of the obstacle
-	 * @return a rectangle in those boundaries
-	 */
-	
-<<<<<<< Updated upstream
-	/**
-	 * Checks if the character reached the total amount of collsions before having to restart
-	 * @return
-	 */
-	public boolean reachTotalCollisions(Caterpillar caterpillar, Butterfly butterfly) {
-		if(caterpillar != null) {
-			if(caterpillar.getTotalCollisions() > 3) {
-				return true;
-			}
-		}
-		
-		if(butterfly != null) {
-			if(butterfly.getTotalCollisions() > 3) {
-				return true;
-			}
-		}
-		
-		return false;
-	}
-	
-	public double getX() {
-		return x;
-	}
-	
-	public double getY() {
-		return y;
-	}
-	
-	public void moveByAmount(double x, double y) {
-		this.x += x;
-		this.y += y;
-	}
-=======
->>>>>>> Stashed changes
 }
