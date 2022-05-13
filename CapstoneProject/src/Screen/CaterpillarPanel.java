83d17e2f87ad96f85c03c4dee3c21ad83f11cc38
Emily Choi <echoi011@student.fuhsd.org>
2022-05-13 10:22:12 -0700

    adjusting leaf locatiion

--- CapstoneProject/src/Screen/CaterpillarPanel.java
+++ CapstoneProject/src/Screen/CaterpillarPanel.java
@@ -54,7 +54,7 @@ public class CaterpillarPanel extends Screen{
 	private void addRandomCollectibles() {
 		for(int i = 0; i < 2; i++)
 		{
-			leaves.add(new Collectible("leaf", "img/Leaf.gif", 5, DRAWING_WIDTH+(i*60), DRAWING_HEIGHT/2));
+			leaves.add(new Collectible("leaf", "img/Leaf.gif", 5, DRAWING_WIDTH+(i*100), DRAWING_HEIGHT/2));
 		}
 	}
 
