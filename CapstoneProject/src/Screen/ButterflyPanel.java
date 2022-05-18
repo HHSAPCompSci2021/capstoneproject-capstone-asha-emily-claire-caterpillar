1d867eb55d09d6e1551807c0498e20b67919dea8
Claire Oh <coh154@student.fuhsd.org>
2022-05-17 20:45:59 -0700

    Added menu thing

--- CapstoneProject/src/Screen/ButterflyPanel.java
+++ CapstoneProject/src/Screen/ButterflyPanel.java
@@ -36,6 +36,8 @@ public class ButterflyPanel extends Screen {
 	 */
 	private Butterfly b;
 	
+	private HealthBar h;
+	
 	
 	//private boolean restart;
 	
@@ -47,13 +49,14 @@ public class ButterflyPanel extends Screen {
 		super(800,600);
 		surface = s;
 		//restart = false;
-		b = new Butterfly(5, 100, 100, "img/ButterflySprite1.gif");
+		b = new Butterfly(5, 100, 100, "img/Butterfly1.gif");
 		obs = new ArrayList<Element>();
-		col = new ArrayList<Collectible>();
+		//col = new ArrayList<Collectible>();
 		int i = 0;
 		addRandomElement();
 		addRandomCollectible();
 		//System.out.println(obs);
+		h = new HealthBar(b);
 	}
 	
 	/**
@@ -67,7 +70,7 @@ public class ButterflyPanel extends Screen {
 			double choice = (Math.random());
 			//System.out.println(choice);
 			int x = (((int)(Math.random() * 200) + 800) / 10) * 10;
-			
+			double speed = 10 + (i * 0.5);
 			if(choice >= 0.9) {
 				y = 64;
 			} else if(choice >= 0.7) {
@@ -81,11 +84,11 @@ public class ButterflyPanel extends Screen {
 			} 
 			
 			if(choice >= 0.5) {
-				obs.add(new Predator("img/Predator.gif", 800 + (i * x), y, 10));
+				obs.add(new Predator("img/Predator.gif", 800 + (i * x), y, speed));
 				//System.out.println(false);
 	
 			} else {
-				obs.add(new Obstacle("img/Kite.gif", 800 + (i * x), y, 10));
+				obs.add(new Obstacle("img/Kite.gif", 800 + (i * x), y, speed));
 				//System.out.println(true);
 	
 			}
@@ -98,7 +101,9 @@ public class ButterflyPanel extends Screen {
 	 */
 	public void addRandomCollectible() {
 
-		for(int i = 0; i < 13; i++) {
+		for(int i = 0; i < 7; i++) {
+			
+			double speed = 10 + (i * 0.5);
 			
 			double choice = (Math.random());
 			int y = 0; 
@@ -116,8 +121,8 @@ public class ButterflyPanel extends Screen {
 			int x = (((int)(Math.random() * 200) + 400) / 10) * 10;
 			
 			//Was this supposed to be added to the collectible array instead of obs
-			obs.add(new Collectible("flower", "img/Flower.gif", 10, 800 + (i * x), y));
-			System.out.println(false);
+			obs.add(new Collectible("flower", "img/Flower.gif", speed, 800 + (i * x), y));
+			//System.out.println(false);
 			
 		}	
 	}
@@ -146,7 +151,7 @@ public class ButterflyPanel extends Screen {
 		surface.background(255,255,255);
 		surface.fill(0);
 		
-		
+		h.draw(surface);
 		if(b.getIsAlive()) {
 			//restart = false;
 			if (surface.isPressed(KeyEvent.VK_UP) && b.getY() >= 0)
@@ -163,25 +168,46 @@ public class ButterflyPanel extends Screen {
 				
 			sideScrolling();
 			
-			for(Collectible m : col) {
-				
-				if(m.collide(b.playerDesignRect())) {
-					m.eat(b);
-				} else {
-					m.draw(surface, 64, 64);
-				}
-				
-					
-			}
+//			for(Collectible m : col) {
+//				
+//				if(m.collide(b.playerDesignRect())) {
+//					System.out.print(true);
+//					m.eat(b);
+//					
+//					//m.moveByAmount(m.getX() + DRAWING_WIDTH, 0);
+//					
+//				} else {
+//					m.draw(surface, 64, 64);
+//				}
+//				
+//					
+//			}
 			
 			
 			for(Element e : obs) {
 				
-				
-				if(e.collide(b.playerDesignRect())) {
-						b.increaseCollisions(e);
+				if(e instanceof Collectible) {
+					if(e.collide(b.playerDesignRect())) {
+						System.out.print(true);
+						
+						
+						e.moveByAmount(e.getX() - DRAWING_WIDTH, 0);
+						((Collectible) e).eat(b);
+					} else {
+						e.draw(surface);
+					}
 				} else {
-					e.draw(surface);	
+					if(e.collide(b.playerDesignRect())) {
+						if(e instanceof Predator) {
+							b.increaseCollisions((Predator)e);
+						} else {
+							e.moveByAmount(e.getX() - 64, DRAWING_HEIGHT);
+							b.increaseCollisions((Obstacle)e);
+						}
+						
+					} else {
+						e.draw(surface);	
+					}
 				}
 									
 						
@@ -209,14 +235,7 @@ public class ButterflyPanel extends Screen {
 	 */
 	public void sideScrolling() {
 		
-		for(Collectible m : col) {
-			if(m.getX() > -64) { 
-
-				m.move();
-				
-				
-			}
-		}
+	
 		
 		for(Element e : obs) {
 			if(e.getX() > -64) { 
